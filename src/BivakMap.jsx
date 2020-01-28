135645dd5efdf0915b76340ecb13cc7e025eb139
prgrmfl <hrn.acc@protonmail.com>
2020-01-28 12:22:42 +0100

    Feature added-Current location marker

--- src/BivakMap.jsx
+++ src/BivakMap.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import {Map as LeafletMap, GeoJSON, Marker, Popup} from 'react-leaflet';
-import bivakzones from './bivakzones.json';
-import { Card, Button } from 'react-bootstrap';
+//import {Map as LeafletMap, GeoJSON, Marker, Popup} from 'react-leaflet';
+//import bivakzones from './bivakzones.json';
+import { Card /*,Button*/ } from 'react-bootstrap';
 
-const BivakMap=({bivakzone})=>{
-    console.log(bivakzone)
-    return (
-      <Card>
-        <Card.Img className="image" variant="top" src={`${bivakzone.properties.image}`} />
-        <Card.Body>
+const BivakMap = ({ bivakzone }) => {
+  console.log(bivakzone);
+  return (
+    <Card>
+      <Card.Img className="image" variant="top" src={`${bivakzone.properties.image}`} />
+      <Card.Body>
         <Card.Title>{bivakzone.properties.name}</Card.Title>
         <Card.Text>
-            <ul>
-                <li>operator:{bivakzone.properties.operator}</li>
-                <li>openfire:{bivakzone.properties.openfire}</li>
-                <li>opening_hours:{bivakzone.properties.opening_hours}</li>
-                <li>reservation:{bivakzone.properties.reservation}</li>
-            </ul>
+          <ul>
+            <li>operator:{bivakzone.properties.operator}</li>
+            <li>openfire:{bivakzone.properties.openfire}</li>
+            <li>opening_hours:{bivakzone.properties.opening_hours}</li>
+            <li>reservation:{bivakzone.properties.reservation}</li>
+          </ul>
         </Card.Text>
-        </Card.Body>
+      </Card.Body>
     </Card>
-    )
+  );
 };
 
 export default BivakMap;
