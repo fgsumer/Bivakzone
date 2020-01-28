08167e20416c66497ec58e573ef214c641736752
berihugebre <danigebre039@gmail.com>
2020-01-28 12:29:48 +0100

    Feat(BivakMap Component)

---
+++ src/Components/BivakMap.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Card} from 'react-bootstrap';
+import { Map as LeafletMap,GeoJSON, TileLayer, Marker, Popup} from 'react-leaflet';
+import { useParams } from 'react-router-dom';
+import bivakzones from '../bivakzones.json'
+
+
+const BivakMap=()=>{
+    const {id}  = useParams();
+    const bivakzone = bivakzones.features.filter((bivakzone)=> bivakzone.id ===`node/${id}`|| bivakzone.id === `way/${id}`);
+    const data = bivakzone[0];
+    let x= 0, y=0;
+    if(data.geometry.type==='Point'){
+         x = data.geometry.coordinates[0];
+         y = data.geometry.coordinates[1];
+    }else {
+         x = data.geometry.coordinates[0][0][0];
+         y = data.geometry.coordinates[0][0][1];
+    }
+    return (
+        <LeafletMap
+          center={[y, x]}
+          zoom={13}
+          maxZoom={19}
+          attributionControl={true}
+          zoomControl={true}
+          doubleClickZoom={true}
+          scrollWheelZoom={true}
+          dragging={true}
+          animate={true}
+          easeLinearity={0.35}
+        >
+          <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'/>
+                <GeoJSON
+                  data={data}
+                  style={() => ({
+                    color: '#4a83ec',
+                    weight: 0.5,
+                    fillColor: "#1a1d62",
+                    fillOpacity: 1,
+                    }
+                    )
+                  } 
+                >
+                  <Popup>
+                    {data.properties.name}
+                  </Popup>
+                </GeoJSON>
+      
+        </LeafletMap>
+    )
+};
+
+export default BivakMap;
