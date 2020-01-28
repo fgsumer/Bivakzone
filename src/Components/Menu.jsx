bf3b6e8a3760371a1ca95c05031642062189efad
berihugebre <danigebre039@gmail.com>
2020-01-28 12:28:38 +0100

    Feat(Menu Component)

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Navbar, Nav}  from 'react-bootstrap';
+
+const Menu =()=>{
+    return(
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="/home">Bivakzones</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="/filter">Filter</Nav.Link>
+                <Nav.Link href="/favorite">Favorites</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+};
+
+export default Menu;
