3a999ec5e77b6585479f270507b9f8797cbe03f0
berihugebre <danigebre039@gmail.com>
2020-02-13 15:46:11 +0100

    fix modal

--- src/Components/Modal/BivakZoneModal.jsx
+++ src/Components/Modal/BivakZoneModal.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import '../../App.css';
-import { Card, Row, Col } from 'antd';
+import { Card,Row, Col } from 'antd';
 import BivakCarousel from './Carousal';
 import Footer from './ModalFooter';
 import './BivakZoneModal.css';
 import { Link } from 'react-router-dom';
 import FavoriteButton from './FavoriteButton';
-
+import CommentForm from '../Bivakzone/CommentForm'
+import Card1 from 'react-bootstrap/Card'
 class BivakZoneModal extends Component {
   state = {
     slide: false,
@@ -40,19 +41,26 @@ class BivakZoneModal extends Component {
                   cover={<BivakCarousel bivakzone={this.props.bivakzone} />}
                 ></Card>
               </Row>
-              <Row>
-                <Col>
+              <Row className="bivak_btn">
+                <Col span={12}>
                   <Link to={`/home/${this.props.bivakzone.id}`}>
-                    <h1>{this.props.bivakzone.properties.name}</h1>
+                    <h4 style={{textAlign:'center'}}>{this.props.bivakzone.properties.name}</h4>
                   </Link>
                 </Col>
-                <Col>
+                <Col span={12}>
                   <FavoriteButton refresh={this.refreshSate} bivakzone={this.props.bivakzone} />
                 </Col>
               </Row>
+              
               <Row style={{ height: 'max-content', borderTop: '1px solid rgb(47, 155, 255)' }}>
                 <Footer properties={this.props.bivakzone.properties}></Footer>
               </Row>
+              <Card1>
+                <Card1.Body>
+                    <Card1.Text><h6 className="fas fa-user-tie mr-2">{this.props.bivakzone.properties.operator}</h6></Card1.Text>
+                    <Card1.Text><h6 className="fas fa-mobile-alt mr-2">{this.props.bivakzone.properties.phone}</h6></Card1.Text>
+                </Card1.Body>
+            </Card1>
             </>
           )}
         </article>
