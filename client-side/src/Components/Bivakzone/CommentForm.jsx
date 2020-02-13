07e27e7da0ddcbc30e3643c785afd88358dc4076
prgrmfl <prgrmfl@protonmail.com>
2020-02-13 04:46:05 +0100

    edited packagejson and serverjs for heroku

--- client-side/src/Components/Bivakzone/CommentForm.jsx
+++ client-side/src/Components/Bivakzone/CommentForm.jsx
@@ -14,7 +14,7 @@ const Comment = ({ id }) => {
 
   const commentGetter = () => {
     axios
-      .get(`http://localhost:5000/api/comment/${id.replace('/', '%2F')}`)
+      .get(`/api/comment/${id.replace('/', '%2F')}`)
       .then(comments => setOldComments(comments))
       .catch(err => console.log(err));
   };
@@ -34,7 +34,7 @@ const Comment = ({ id }) => {
     e.target.reset();
     axios
       .post(
-        `http://localhost:5000/api/comment`,
+        `/api/comment`,
         { id, name, message },
         {
           headers: { 'Content-Type': 'application/json' },
--- server.js
+++ server.js
@@ -46,6 +46,10 @@ app.get('/api/comment/:id', async (req, res) => {
   }
 });
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client-side/build')));
+
+// Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client-side/build/index.html'));
 });
