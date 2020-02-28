93ae2a49491c63c7646386e1bd21e06b7eeca306
prgrmfl <prgrmfl@protonmail.com>
2020-02-28 22:19:25 +0100

    update build

---
+++ client-side/build/precache-manifest.ecea1b5aae9f516994b6dc0edbb1f6b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a0e7c8b5adc4f3dbed6985dd3dc95e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ac1285f757d40883ffc",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "576fbd4ee04c4a274944",
+    "url": "/static/css/main.35401eb9.chunk.css"
+  },
+  {
+    "revision": "4ac1285f757d40883ffc",
+    "url": "/static/js/2.079ae16a.chunk.js"
+  },
+  {
+    "revision": "89cb33879b02831230f1d02133ff3728",
+    "url": "/static/js/2.079ae16a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "576fbd4ee04c4a274944",
+    "url": "/static/js/main.0da9ab8f.chunk.js"
+  },
+  {
+    "revision": "192a71dc801613e28029",
+    "url": "/static/js/runtime-main.67745738.js"
+  }
+]);
--- client-side/build/service-worker.js
+++ client-side/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Berihugebre/BootCamp-Bivakzone/precache-manifest.d75c09c9b9bce2af10f260486f55a03f.js"
+  "/precache-manifest.ecea1b5aae9f516994b6dc0edbb1f6b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Berihugebre/BootCamp-Bivakzone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
